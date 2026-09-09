07edcf48f31b26bf40d9c4d4d2aa456e8c0ac9c2
Aradhya-Tripathi <aradhya@frappe.io>
2026-09-09 16:00:59 +0530

    refactor: Drop settings single doctype

--- cargo/telemetry/doctype/datum_settings/datum_settings.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Aradhya-Tripathi and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Datum Settings", {
-// 	refresh(frm) {
-
-// 	},
-// });
