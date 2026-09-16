392f1aa141472745d2898e97aeb4ff2dd4de10ee
Aradhya-Tripathi <aradhya@frappe.io>
2026-09-16 23:45:24 +0530

    chore: Adapt datum spawning with singles refactor

--- cargo/testing.py
+++ cargo/testing.py
@@ -1,5 +1,5 @@
 import frappe
-from frappe.utils.password import set_encrypted_password
+from frappe.utils.password import remove_encrypted_password, set_encrypted_password
 
 SETTINGS = {
 	"central_url": "http://central.test",
@@ -19,6 +19,7 @@ SECRETS = {
 	"central_webhook_secret": "test-webhook-secret",
 	"proxy_token": "test-proxy-token",
 }
+DATUM_SECRETS = ("clickhouse_password", "insights_password", "default_password")
 
 
 def use_test_settings() -> None:
@@ -33,3 +34,13 @@ def use_test_settings() -> None:
 		set_encrypted_password("Cargo Settings", "Cargo Settings", secret, field)
 
 	frappe.clear_document_cache("Cargo Settings", "Cargo Settings")
+
+
+def reset_datum_server() -> None:
+	"""Clear the region's datum host between tests. It is a Single, so there is no row to
+	delete, and its secrets live apart from the values they belong to."""
+	frappe.db.delete("Singles", {"doctype": "Datum Server"})
+	for field in DATUM_SECRETS:
+		remove_encrypted_password("Datum Server", "Datum Server", field)
+
+	frappe.clear_document_cache("Datum Server", "Datum Server")
